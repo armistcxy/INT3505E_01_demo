c2bbfd73c69d93e17bd859e63838e0bfe6f6a08e
Ngô Lê Hoàng <99264715+armistcxy@users.noreply.github.com>
2025-10-01 21:35:49 +0700

    fix: remove column is_available, use book_counts instead

--- demo_02/app/routes/books.py
+++ demo_02/app/routes/books.py
@@ -7,14 +7,13 @@ books_bp = Blueprint("books", __name__)
 @books_bp.route("/", methods=["GET"])
 def get_books():
     books = Book.query.all()
-    return jsonify([{"id": b.id, "title": b.title, "author": b.author, "is_available": b.is_available} for b in books])
+    return jsonify([{"id": b.id, "title": b.title, "author": b.author} for b in books])
 
 @books_bp.route("/<int:book_id>", methods=["GET"])
 def get_book(book_id):
     book = Book.query.get_or_404(book_id)
     return jsonify({"id": book.id, "title": book.title, 
                     "author": book.author,
-                    "is_available": book.is_available,
                     "publication_year": book.publication_year,
                     "genre": book.genre
                     })
@@ -24,7 +23,6 @@ def add_book():
     data = request.json
     book = Book(title=data["title"], 
                 author=data.get("author"),
-                is_available=data.get("is_available"), 
                 publication_year=data.get("publication_year"), 
                 genre=data.get("genre")
                 )
@@ -42,9 +40,6 @@ def update_book(book_id):
     
     if "author" in data and data["author"]:  
         book.author = data["author"]
-        
-    if "is_available" in data and data["is_available"] is not None:  
-        book.is_available = bool(data["is_available"])
     
     if "publication_year" in data and data["publication_year"]:  
         book.publication_year = int(data["publication_year"])
